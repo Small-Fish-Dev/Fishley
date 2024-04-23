ad58d70c37c04282642382ce39331cba1aa4608b
ubre <ubermedick@gmail.com>
2024-04-23 22:45:48 +0200

    View most reported fish, view fish by id

--- Code/Commands/Fishley.Commands.Fish.cs
+++ Code/Commands/Fishley.Commands.Fish.cs
@@ -9,7 +9,7 @@ public partial class Fishley
 		.WithDescription("Access the fish database")
 		.AddOption(new SlashCommandOptionBuilder()
 			.WithName("edit")
-			.WithDescription("Edit a fish")
+			.WithDescription("Edit a fish (This clears the reported issues count too)")
 			.WithType(ApplicationCommandOptionType.SubCommand)
 			.AddOption(new SlashCommandOptionBuilder()
 				.WithName("fish_id")
@@ -93,6 +93,17 @@ public partial class Fishley
 			)
 		)
 		.AddOption(new SlashCommandOptionBuilder()
+			.WithName("view")
+			.WithDescription("View a fish")
+			.WithType(ApplicationCommandOptionType.SubCommand)
+			.AddOption(new SlashCommandOptionBuilder()
+				.WithName("fish_id")
+				.WithDescription("Which fish to view")
+				.WithRequired(true)
+				.WithType(ApplicationCommandOptionType.Integer)
+			)
+		)
+		.AddOption(new SlashCommandOptionBuilder()
 			.WithName("add")
 			.WithDescription("Add a fish")
 			.WithType(ApplicationCommandOptionType.SubCommand)
@@ -108,6 +119,11 @@ public partial class Fishley
 				.WithRequired(true)
 				.WithType(ApplicationCommandOptionType.String)
 			)
+		)
+		.AddOption(new SlashCommandOptionBuilder()
+			.WithName("most_issues")
+			.WithDescription("Return a list of fishes with the most issues reported")
+			.WithType(ApplicationCommandOptionType.SubCommand)
 		)
 		.WithDefaultMemberPermissions(GuildPermission.Administrator);
 
@@ -186,6 +202,21 @@ public partial class Fishley
 						DebugSay($"Removed from database fish: {fishData.WikiPage}");
 					}
 					break;
+				case "view":
+					{
+						var fishId = (int)(long)commandType.Options.First().Value;
+						var fishData = await GetFish(fishId);
+
+						if (fishData == null)
+						{
+							await command.RespondAsync($"Could not find any fish with the id {fishId}.", ephemeral: true);
+							return;
+						}
+
+						var embed = new FishEmbedBuilder(fishData).Build();
+						await command.RespondAsync(embed: embed);
+					}
+					break;
 				case "add":
 					{
 						var wikiPage = (string)commandType.Options.First().Value;
@@ -205,6 +236,33 @@ public partial class Fishley
 						var addedFish = await GetFish(fishResponse.Item3);
 						var embed = new FishEmbedBuilder(addedFish, "Fish has been added").Build();
 						await SendMessage(channel, $"<@{command.User.Id}>", embed: embed);
+					}
+					break;
+				case "most_issues":
+					{
+						using (var db = new FishleyDbContext())
+						{
+							var fishes = db.Fishes;
+							var foundFishes = fishes.AsAsyncEnumerable();
+							var mostIssues = foundFishes.OrderByDescending(x => x.IssuesReported);
+							var top5 = await mostIssues.Take(5).ToListAsync();
+
+							var embedBuilder = new EmbedBuilder().WithTitle("Fishes with most issues reported")
+								.WithCurrentTimestamp();
+
+							var spot = 1;
+							foreach (var fish in top5)
+							{
+								if (fish.IssuesReported <= 0) break;
+								embedBuilder = embedBuilder.AddField($"#{spot}: {fish.CommonName}", $"Times Reported: {fish.IssuesReported} - Fish Id: {fish.PageId}");
+								spot++;
+							}
+
+							if (spot == 1)
+								await command.RespondAsync("No issues found, good job team :-)");
+							else
+								await command.RespondAsync(embed: embedBuilder.Build());
+						}
 					}
 					break;
 			}
