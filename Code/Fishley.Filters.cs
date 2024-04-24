60b88c8a4bf0035916cb9284be6cd9253b595a58
ubre <ubermedick@gmail.com>
2024-04-24 22:33:40 +0200

    Added discord link filter warn

--- Code/Fishley.Filters.cs
+++ Code/Fishley.Filters.cs
@@ -2,13 +2,26 @@ namespace Fishley;
 
 public partial class Fishley
 {
-	private static async Task<bool> HandleFilters( SocketUserMessage message )
+	private static async Task<bool> HandleFilters(SocketUserMessage message)
 	{
-		if ( await HandleSimpleFilter( message ) || 
-		await HandleComplicatedFilter( message ) ||
-		await HandleConfusingFilter( message ) )
+		if (await HandleSimpleFilter(message)) return true;
+		if (await HandleComplicatedFilter(message)) return true;
+		if (await HandleConfusingFilter(message)) return true;
+		if (await HandleDiscordInviteFilter(message)) return true;
+
+		return false;
+	}
+
+	private static async Task<bool> HandleDiscordInviteFilter(SocketUserMessage message)
+	{
+		var regex = new Regex($@"(https?:\/\/|http?:\/\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\/invite|discord.com\/invite)\/[^\s\/]+?(?=\b)"); // Idk I can't put it in json so I'll just keep it here
+		if (regex.Match(message.Content).Success)
+		{
+			var user = message.Author as SocketGuildUser;
+
+			await AddWarn(user, message, $"Hey <@{user.Id}>, don't go crazy with the invite links!");
 			return true;
-		
+		}
 		return false;
 	}
 
@@ -18,10 +31,10 @@ public partial class Fishley
 	/// <param name="message"></param>
 	/// <returns></returns>
 	private static async Task<bool> HandleSimpleFilter(SocketUserMessage message)
-    {
-		string pattern = ConfigGet<string>( "SimpleRegex" );
-		
-		return await HandleFilter( message, pattern, "shrimple" );
+	{
+		string pattern = ConfigGet<string>("SimpleRegex");
+
+		return await HandleFilter(message, pattern, "shrimple");
 	}
 
 	/// <summary>
@@ -30,10 +43,10 @@ public partial class Fishley
 	/// <param name="message"></param>
 	/// <returns></returns>
 	private static async Task<bool> HandleComplicatedFilter(SocketUserMessage message)
-    {
-		string pattern = ConfigGet<string>( "ComplicatedRegex" );
-		
-		return await HandleFilter( message, pattern, "clamplicated" );
+	{
+		string pattern = ConfigGet<string>("ComplicatedRegex");
+
+		return await HandleFilter(message, pattern, "clamplicated");
 	}
 
 	/// <summary>
@@ -42,10 +55,10 @@ public partial class Fishley
 	/// <param name="message"></param>
 	/// <returns></returns>
 	private static async Task<bool> HandleConfusingFilter(SocketUserMessage message)
-    {
-		string pattern = ConfigGet<string>( "ConfusingRegex" );
-		
-		return await HandleFilter( message, pattern, "conchfusing" );
+	{
+		string pattern = ConfigGet<string>("ConfusingRegex");
+
+		return await HandleFilter(message, pattern, "conchfusing");
 	}
 
 	/// <summary>
@@ -57,11 +70,11 @@ public partial class Fishley
 	/// <returns></returns>
 	private static async Task<bool> HandleFilter(SocketUserMessage message, string regexPattern, string correctWord)
 	{
-		if ( FindAndReplace( message.Content, regexPattern, correctWord, out string correctedMessage ))
+		if (FindAndReplace(message.Content, regexPattern, correctWord, out string correctedMessage))
 		{
 			var user = message.Author as SocketGuildUser;
 
-			await AddWarn( user, message, $"Hey <@{user.Id}>, perhaps you meant to say: \n> {correctedMessage} \n" );
+			await AddWarn(user, message, $"Hey <@{user.Id}>, perhaps you meant to say: \n> {correctedMessage} \n");
 			return true;
 		}
 		return false;
@@ -77,36 +90,36 @@ public partial class Fishley
 	/// <param name="lettersAround"></param>
 	/// <param name="excludeLinks"></param>
 	/// <returns></returns>
-	private static bool FindAndReplace( string messageToCheck, string regexPattern, string correctWord, out string correctedMessage, int lettersAroundIncluded = 10, bool excludeLinks = true )
+	private static bool FindAndReplace(string messageToCheck, string regexPattern, string correctWord, out string correctedMessage, int lettersAroundIncluded = 10, bool excludeLinks = true)
 	{
 		correctedMessage = "";
-		var regex = new Regex( regexPattern, RegexOptions.IgnoreCase );
-		var match = regex.Match( messageToCheck );
+		var regex = new Regex(regexPattern, RegexOptions.IgnoreCase);
+		var match = regex.Match(messageToCheck);
 
-		if ( match.Success )
+		if (match.Success)
 		{
 			var matchingIndex = match.Index;
 			var matchingWord = match.Value;
-			
-			if ( excludeLinks )
+
+			if (excludeLinks)
 			{
-				var linkRegex = new Regex( @"(?:https?://)?(?:www\.)?\S+\.\S+", RegexOptions.IgnoreCase );
-				var linkMatch = linkRegex.Match( messageToCheck );
+				var linkRegex = new Regex(@"(?:https?://)?(?:www\.)?\S+\.\S+", RegexOptions.IgnoreCase);
+				var linkMatch = linkRegex.Match(messageToCheck);
 				var linkStart = linkMatch.Index;
 				var linkEnd = linkStart + linkMatch.Value.Length;
 
-				if ( matchingIndex >= linkStart && matchingIndex + matchingWord.Length <= linkEnd ) // The match is inside a link
+				if (matchingIndex >= linkStart && matchingIndex + matchingWord.Length <= linkEnd) // The match is inside a link
 					return false;
 			}
 
-            var startIndex = Math.Max(0, matchingIndex - lettersAroundIncluded);
-            var endIndex = Math.Min( messageToCheck.Length, matchingIndex + matchingWord.Length + lettersAroundIncluded );
+			var startIndex = Math.Max(0, matchingIndex - lettersAroundIncluded);
+			var endIndex = Math.Min(messageToCheck.Length, matchingIndex + matchingWord.Length + lettersAroundIncluded);
 			var length = endIndex - startIndex;
 			var isStarting = startIndex == 0;
 			var isEnding = endIndex == messageToCheck.Length;
 
-            var phrase = messageToCheck.Substring( startIndex, length );
-			var replacedPhrase = phrase.Replace( matchingWord, $"~~{matchingWord}~~ **{correctWord}**");
+			var phrase = messageToCheck.Substring(startIndex, length);
+			var replacedPhrase = phrase.Replace(matchingWord, $"~~{matchingWord}~~ **{correctWord}**");
 
 			correctedMessage = $"{(isStarting ? "" : "...")}{replacedPhrase}{(isEnding ? "" : "...")}";
 			return true;
