742226bd79578ab912d33becda6ff8d771dc1ce8
TheOwlOfLife <lportalthe@gmail.com>
2024-05-21 00:40:27 +0300

    Changed the way getting username works

---
+++ Code/Util/Extensions/DiscordIUserExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Fishley
+{
+    public static class DiscordIUserExtensions
+    {
+        public static string GetUsername(this IUser user) => user.GlobalName != null ? user.GlobalName : user.Username;
+    }
+}
