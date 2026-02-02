0e96e108052d4ef11bd94012562a8f96c6849564
ubre <ubermedick@gmail.com>
2026-02-02 20:51:38 +0100

    Disable annoying youtube debug

--- Code/Scraping/Fishley.Scraping.Youtube.cs
+++ Code/Scraping/Fishley.Scraping.Youtube.cs
@@ -29,7 +29,6 @@ public partial class Fishley
 
 		public override async Task<ScrapingResult> Fetch()
 		{
-			DebugSay( Url );
 			using (HttpClient client = new HttpClient())
 			{
 				client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
